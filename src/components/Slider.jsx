bfdda4e28a0b1a45a9984916c9a9fcf7dce024ef
JamesPesenti <74566532+JamesPesenti@users.noreply.github.com>
2021-09-11 16:03:35 -0400

    Add files via upload

---
+++ src/components/Slider.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Slider from "./Slider.css";
+import ScrollAnimation from "react-animate-on-scroll";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+AOS.init(); {
+    // or simply just AOS.init();
+    AOS.init({
+      // initialise with other settings
+      duration : 200
+    });
+  }
+
+function S(){
+    return (
+        
+<div>
+{/* <ScrollAnimation animateIn="fade"> */}
+<div className="slider" data-aos="zoom-in">
+    <div className="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+            <div className="carousel-item active">
+                <h3 className="slideH3">A Little About Me</h3>
+                <br/>
+                <br/>
+                <h7>I'm Pedro Guerra from Chicago Illinois and I have a </h7>
+                <h7>passion for using my abilities to help those that </h7>
+                <h7>are searching for the clarity that they need in their life.</h7>
+            </div>
+            <div className="carousel-item">
+                <h3 className="slideH3">My Abilities</h3>
+                <br/>
+                <br/>
+                <h7> I have been developing my clairvoyance since I discovered I had a gift </h7>
+                <h7> as a teen. I started out mostly just guiding my friends and family with the </h7>
+                <h7> insights I gained from visions that I was having about them. </h7>
+            </div>
+
+            <div className="carousel-item">
+                <h3 className="slideH3">Experience</h3>
+                <br/>
+                <br/>
+                <h7> My journey has now evolved into doing over 1500 readings all across the country & world. <i
+                        className="fas fa-globe-americas"></i></h7>
+            </div>
+                
+
+
+            <div className="carousel-item">
+                <h3 className="slideH3">My Services</h3>
+                <br/>
+                <h7> Readings | Chakra Allignment | Tarot Card Readings | </h7>
+                <h7>Energy Healing Sessions | Numerology</h7>
+                <br/>
+                <br/>
+                <img class="d-block w-100" src="..." alt=""/>
+        </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+ {/* </ScrollAnimation> */}
+
+
+        <div className="button1">
+            <a class="c-social-links__link"
+                href="https://jamespesenti.github.io/InnerAuraReadings/chakras.html">
+                <btn btn btn-primary></btn>
+            </a>
+        </div>
+        <div className="button11">
+            <a class="c-social-links__link"
+                href="https://jamespesenti.github.io/InnerAuraReadings/chakras.html">
+                <btn btn btn-primary></btn>
+            </a>
+        </div>
+
+</div>
+
+
+    )
+}
+
+export default S;
+
---
+++ src/components/Testimonials2.jsx
@@ -0,0 +1,67 @@
+// import React from "react";
+// import $ from 'jquery';
+// import Testimonials from "./Testimonials.css";
+
+
+// function T(){
+//     return (
+
+
+// <div>
+// <div className="Testimonials" data-aos="zoom-in">
+//     <div className="gtco-testimonials" id={{Testimonials}}>
+//         <h1 style={{color: "#01b0f8"}}>Testimonials</h1>
+//         <div className="tSlide">
+//     <div className="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+//         <div className="carousel-inner">
+//             <div className="carousel-item active">
+//             <h1>Lorena</h1>
+//             <div className="card text-center"><img className="card-img-top" src="pictures/lorena.jpg" alt=""/>
+//                         <div className="card-body">
+//             </div>
+//         </div>
+//     </div>
+//     </div>
+
+
+//         <div>
+//             <div className="carousel-item">
+//                 <div className="card text-center"><img className="card-img-top" src="pictures/unnamed.jpg" alt=""/>
+//                         <div className="card-body">
+//                         ////
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <div className="carousel-item">
+//                     ////
+//                         </div>
+                
+    
+    
+
+
+//  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+//             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+//             <span className="sr-only">Previous</span>
+//         </a>
+//         <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+//             <span className="carousel-control-next-icon" aria-hidden="true"></span>
+//             <span className="sr-only">Next</span>
+//  </a>
+
+// </div>
+// </div>
+// </div>
+// </div>
+// </div>
+// </div>
+
+//     );
+// }
+
+
+
+
+// export default T;
