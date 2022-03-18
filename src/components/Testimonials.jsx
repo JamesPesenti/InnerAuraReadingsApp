e635050231d6b8348ab42a752f3ba6b1bec8e9e1
James Pesenti <74566532+JamesPesenti@users.noreply.github.com>
2022-03-18 11:54:20 -0400

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,69 +7,94 @@ import Testimonials from "./Testimonials.css";
 
 function Test(){
     return (
-    <div>
-        <div className="Testimonials">
-            <div className="gtco-testimonials" id={{Testimonials}}>
-                <h1 style={{color: "#01b0f8"}}>Testimonials</h1>
-            <div className="about">
-                <div>
-                    <div className="card text-center"><img className="card-img-top" src="pictures/lorena.jpg" alt=""/>
-                        <div className="card-body">
-                            <h5>Lorena<br />
-                                <span></span></h5>
-                            <p className="card-text"> "" Pedro is always available to return my texts and extremely
-                                flexible scheduling reading times! His knowledge and understanding of astrological
-                                transitions are on point each time we have talked. Each reading has been amazingly
-                                clear with soon happening predictions. Talking to Pedro is like talking to my spirit
-                                angels! "" </p>
-                        </div>
+//     <div>
+//         <div className="Testimonials">
+//             <div className="gtco-testimonials" id={{Testimonials}}>
+//                 <h1 style={{color: "#01b0f8"}}>Testimonials</h1>
+//             <div className="about">
+//                 <div>
+//                     <div className="card text-center"><img className="card-img-top" src="pictures/lorena.jpg" alt=""/>
+//                         <div className="card-body">
+//                             <h5>Lorena<br />
+//                                 <span></span></h5>
+//                             <p className="card-text"> "" Pedro is always available to return my texts and extremely
+//                                 flexible scheduling reading times! His knowledge and understanding of astrological
+//                                 transitions are on point each time we have talked. Each reading has been amazingly
+//                                 clear with soon happening predictions. Talking to Pedro is like talking to my spirit
+//                                 angels! "" </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <div className="card text-center"><img className="card-img-top" src="pictures/unnamed.jpg" alt=""/>
+//                         <div className="card-body">
+//                             <h5>Jennifer C<br />
+//                                 <span></span></h5>
+//                             <p className="card-text">"" Besides being a gifted psychic, Pedro is a warm person and very
+//                                 easy to talk to. Pedro is positive and it also feels like he is offering coaching in
+//                                 my life. ""</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <div className="card text-center"><img className="card-img-top" src="pictures/christina.JPG" alt=""/>
+//                         <div className="card-body">
+//                             <h5>Christina F<br />
+//                                 <span></span></h5>
+//                             <p className="card-text">"" I have been doing readings with Pedro for almost three years and
+//                                 every reading is better than the last.
+//                                 I have gotten readings done by others before but there is no comparison.
+//                                 He is so accurate and descriptive! He makes you feel positive and hopeful.
+//                                 I have recommended him to several friends and family members of mine, that have all
+//                                 been blown away and pleased with their readings as well.
+//                                 He guides you to spiritual awakening and your utmost potential. He is a true
+//                                 blessing. ""</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <div className="card text-center"><img className="card-img-top" src="pictures/4th.jpg" alt=""/>
+//                         <div className="card-body">
+//                             <h5>Jacqueline<br />
+//                                 <span></span></h5>
+//                             <p className="card-text">"" Words can’t express how amazing and accurate Pedro was! We hadn't
+//                                 met or talked before my reading and
+//                                 the one thing that I will always remember is that Pedro told me to ask for
+//                                 validation and that my spirit guide will
+//                                 show me what I need to know. I’m looking forward to my future that Pedro predicted! ""
+//                             </p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+        
+        
+        
+        
+//         new testimonial slider
+        
+        
+        <div className="w-84 carousel rounded-box rounded m-8">
+        <div className="carousel-item w-full">
+            <div className="container pt-10 mr-5">
+                <div className="card w-76 bg-base-100 shadow-xl bg-cyan-50">
+                        <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title justify-center">Lorena</h2>
+                             <p>"Pedro is always available to return my texts and extremely flexible scheduling reading times! His knowledge and understanding of astrological transitions are on point every time we have talked.</p> 
+                             <p> Each reading has been amazingly clear with soon happening predictions. Talking to Pedro is like talking to my spirit angels!"</p>
+                        {/* <div class="card-actions justify-left">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div> */}
                     </div>
                 </div>
-                <div>
-                    <div className="card text-center"><img className="card-img-top" src="pictures/unnamed.jpg" alt=""/>
-                        <div className="card-body">
-                            <h5>Jennifer C<br />
-                                <span></span></h5>
-                            <p className="card-text">"" Besides being a gifted psychic, Pedro is a warm person and very
-                                easy to talk to. Pedro is positive and it also feels like he is offering coaching in
-                                my life. ""</p>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="card text-center"><img className="card-img-top" src="pictures/christina.JPG" alt=""/>
-                        <div className="card-body">
-                            <h5>Christina F<br />
-                                <span></span></h5>
-                            <p className="card-text">"" I have been doing readings with Pedro for almost three years and
-                                every reading is better than the last.
-                                I have gotten readings done by others before but there is no comparison.
-                                He is so accurate and descriptive! He makes you feel positive and hopeful.
-                                I have recommended him to several friends and family members of mine, that have all
-                                been blown away and pleased with their readings as well.
-                                He guides you to spiritual awakening and your utmost potential. He is a true
-                                blessing. ""</p>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="card text-center"><img className="card-img-top" src="pictures/4th.jpg" alt=""/>
-                        <div className="card-body">
-                            <h5>Jacqueline<br />
-                                <span></span></h5>
-                            <p className="card-text">"" Words can’t express how amazing and accurate Pedro was! We hadn't
-                                met or talked before my reading and
-                                the one thing that I will always remember is that Pedro told me to ask for
-                                validation and that my spirit guide will
-                                show me what I need to know. I’m looking forward to my future that Pedro predicted! ""
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+            </div> 
+        </div> 
+        
+        
 
     )};
     
